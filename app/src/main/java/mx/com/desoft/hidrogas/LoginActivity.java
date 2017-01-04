83b9381b8c8dcdbbef1f6a7d2df78bd7f3dd73d8
DeiVeloper <david.c13@hotmail.com>
2017-01-03 22:37:21 -0600

    Modificación al activity del login.

--- app/src/main/java/mx/com/desoft/hidrogas/LoginActivity.java
+++ app/src/main/java/mx/com/desoft/hidrogas/LoginActivity.java
@@ -5,9 +5,12 @@ package mx.com.desoft.hidrogas;
  */
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,59 +27,86 @@ import mx.com.desoft.hidrogas.model.Empleado;
 
 public class LoginActivity extends AppCompatActivity {
 
-    AdminSQLiteOpenHelper bd;
-    Activity activity = this;
+    SharedPreferences preferences;
+    AdminSQLiteOpenHelper dataBase;
+
+    private EditText editTextUsuario;
+    private EditText editTextPassword;
+    private Button btnLogin;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        preferences = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
 
-        bd = new AdminSQLiteOpenHelper(this);
-        //TextView registerScreen = (TextView) findViewById(R.id.btnLinkToRegisterScreen);
-        Button btnLogin = (Button) findViewById(R.id.btnLogin);
-        Button btnAgregarRegistro = (Button) findViewById(R.id.btnAgregarRegistro);
-
-
+        bindUI();
         btnLogin.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
-                List<Empleado> empleado = bd.getContact();
-                for (Empleado row: empleado){
+                String usuario = editTextUsuario.getText().toString();
+                String password = editTextPassword.getText().toString();
+                if(isFormValid(usuario,password)){
+                    goToMain();
+                    saveOnPreferences(usuario,password);
 
-                    Log.d("Datos: ", row.toString());
-                    //Toast toast = Toast.makeText(activity,"DAtos : " + row.toString(), Toast.LENGTH_LONG);
-                    //toast.show();
                 }
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
             }
         });
 
-        btnAgregarRegistro.setOnClickListener(new View.OnClickListener() {
+        /*btnAgregarRegistro.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
 
-                EditText usuario = (EditText) findViewById(R.id.email);
-                EditText password = (EditText) findViewById(R.id.password);
+
                 bd.addContact(usuario.getText().toString(),password.getText().toString());
 
                 Toast toast = Toast.makeText(activity, "Usuario:" + usuario.getText() + " Password: " + password.getText(), Toast.LENGTH_LONG);
                 toast.show();
             }
-        });
-
-
-        // Listening to register new account link
-        /*registerScreen.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                // Switching to Register screen
-                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(i);
-            }
         });*/
     }
+
+    private void saveOnPreferences(String usuario, String password){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("usuario", usuario);// Usuario que se enceuntra en la base de datos y es administrador
+        editor.putString("password", password); // contraseña del usuario logueado
+        editor.apply();
+    }
+
+    private void conectarBaseDeDatos(){
+        dataBase = new AdminSQLiteOpenHelper(this);
+    }
+
+    private void bindUI(){
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        editTextUsuario = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+    }
+
+    private void goToMain(){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private boolean isFormValid(String usuario, String password){
+        if (TextUtils.isEmpty(usuario)){
+            Toast.makeText(this, "El Usuario no puede ir vacio, favor de validar", Toast.LENGTH_LONG).show();
+            return false;
+
+        }   else if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "La contraseña no puede ir vacia, favor de validar", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return getUsarioLogin(usuario,password);
+    };
+
+    private boolean getUsarioLogin(String usuario, String password){
+        // consulta para traer el usuario de la base de datos
+        return true;
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
