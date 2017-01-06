20ccb738743153f2e2c22c183c7fe43c864cf991
cirek18 <cirek_18@hotmail.com>
2017-01-06 11:38:35 -0600

    Se agregaron métodos

--- app/src/main/java/mx/com/desoft/hidrogas/bussines/PersonalBussines.java
+++ app/src/main/java/mx/com/desoft/hidrogas/bussines/PersonalBussines.java
@@ -1,8 +1,10 @@
 package mx.com.desoft.hidrogas.bussines;
 
 import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.view.ViewGroup;
+import android.widget.Toast;
 
 import mx.com.desoft.SQLite.AdminSQLiteOpenHelper;
 import mx.com.desoft.hidrogas.to.PersonalTO;
@@ -13,13 +15,13 @@ import mx.com.desoft.hidrogas.to.PersonalTO;
 
 public class PersonalBussines {
     private static AdminSQLiteOpenHelper baseDatos;
-
+    private Cursor registros;
     public PersonalBussines() {
 
     }
 
-    public void guardar(ViewGroup viewGroup, PersonalTO personalTO) {
-        baseDatos = new AdminSQLiteOpenHelper(viewGroup.getContext());
+    public void guardar(Context context, PersonalTO personalTO, boolean flgEditar) {
+        baseDatos = new AdminSQLiteOpenHelper(context, "hidroGas", null, 1);
         SQLiteDatabase bd = baseDatos.getWritableDatabase();
 
         ContentValues registro = new ContentValues();
@@ -32,7 +34,32 @@ public class PersonalBussines {
         registro.put("fechaRegistro", personalTO.getFechaRegistro());
         registro.put("nominaRegistro", personalTO.getNominaRegistro());
         registro.put("tipoEmpleado", personalTO.getTipoEmpleado());
-        bd.insert("empleados", null, registro);
+        if (flgEditar) {
+            bd.update("empleados", registro, "nominaEmpleado = " + personalTO.getNomina(), null);
+        } else {
+            bd.insert("empleados", null, registro);
+        }
         bd.close();
+    }
+
+    public Cursor buscar(Context context, PersonalTO personalTO) {
+        String condicion = "";
+        baseDatos = new AdminSQLiteOpenHelper(context, "hidroGas", null, 1);
+        SQLiteDatabase bd = baseDatos.getWritableDatabase();
+        if (!personalTO.getNomina().equals("")) {
+            condicion += " AND nominaEmpleado = '" + personalTO.getNomina() + "' ";
+        }
+        if (!personalTO.getNombre().equals("")) {
+            condicion += " AND nombre = '" + personalTO.getNombre() + "' ";
+        }
+        registros = bd.rawQuery("SELECT * FROM empleados WHERE 1=1 " + condicion, null);
+        Toast.makeText(context, condicion, Toast.LENGTH_LONG).show();
+        return registros;
+    }
+
+    public void eliminar(Context context, PersonalTO personalTO) {
+        baseDatos = new AdminSQLiteOpenHelper(context, "hidroGas", null, 1);
+        SQLiteDatabase bd = baseDatos.getWritableDatabase();
+        bd.delete("Empleados", "nominaEmpleado = " + personalTO.getNomina(), null);
     }
 }
