b33fd1e8ec9b26ed6ebc09bfe58bbc0ac1568bee
cirek18 <cirek_18@hotmail.com>
2017-01-16 18:11:53 -0600

    Add files via upload

--- app/src/main/java/mx/com/desoft/SQLite/AdminSQLiteOpenHelper.java
+++ app/src/main/java/mx/com/desoft/SQLite/AdminSQLiteOpenHelper.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -26,14 +25,16 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
             "nominaRegistro TEXT NOT NULL, tipoEmpleado INTEGER NOT NULL DEFAULT 1)";
 
     private static final String SQL_CREAR_LIQUIDACION = "CREATE TABLE Liquidacion(idLiquidacion INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            "nominaChofer TEXT NOT NULL, nominaAyudante TEXT NOT NULL, noPipa INTEGER NOT NULL DEFAULT 0, alerta INTEGER NULL DEFAULT 0" +
-            "variacion INTEGER NULL DEFAULT 0, fechaRegistro INTEGER NOT NULL, nominaRegistro TEXT NOT NULL)";
+            "nominaChofer TEXT NOT NULL, nominaAyudante TEXT NOT NULL, noPipa INTEGER NOT NULL DEFAULT 0," +
+            "variacion INTEGER NOT NULL DEFAULT 0, fechaRegistro INTEGER NOT NULL, nominaRegistro TEXT NOT NULL)";
 
     private static final String SQL_CREAR_LLENADO = "CREATE TABLE Llenado(idLlenado INTEGER PRIMARY KEY AUTOINCREMENT, noPipa INTEGER NOT NULL," +
             "porcentajeLlenado INTEGER NOT NULL, fechaRegistro INTEGER NOT NULL, nominaRegistro TEXT NOT NULL)";
 
     private static final String SQL_CREAR_PIPAS = "CREATE TABLE Pipas (noPipa INTEGER PRIMARY KEY AUTOINCREMENT, fechaRegistro INTEGER NOT NULL," +
             "nominaRegistro TEXT NOT NULL)";
+
+    private static final String SQL_CREAR_TIPOEMPLEADOS = "CREATE TABLE TipoEmpleados (idEmpleado INTEGER PRIMARY KEY, descripcion TEXT)";
 
     private static final String SQL_CREAR_VIAJES = "CREATE TABLE Viajes (idViaje INTEGER PRIMARY KEY AUTOINCREMENT, idLiquidacion INTEGER NOT NULL," +
             "porcentajeInicial INTEGER NOT NULL DEFAULT 0, totalizadorInicial INTEGER NOT NULL DEFAULT 0, porcentajeFinal INTEGER NOT NULL DEFAULT 0," +
@@ -50,6 +51,20 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
         db.execSQL(SQL_CREAR_LLENADO);
         db.execSQL(SQL_CREAR_PIPAS);
         db.execSQL(SQL_CREAR_VIAJES);
+        db.execSQL(SQL_CREAR_TIPOEMPLEADOS);
+
+        ContentValues registro = new ContentValues();
+        registro.put("idEmpleado", 0);
+        registro.put("descripcion", "Administrador");
+        db.insert("tipoEmpleados", null, registro);
+        registro = new ContentValues();
+        registro.put("idEmpleado", 1);
+        registro.put("descripcion", "Chofer");
+        db.insert("tipoEmpleados", null, registro);
+        registro = new ContentValues();
+        registro.put("idEmpleado", 2);
+        registro.put("descripcion", "Ayudante");
+        db.insert("tipoEmpleados", null, registro);
     }
 
     public void addContact(String usuario, String password) {
@@ -63,7 +78,7 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
         registro.put("noPipa", 1);
         registro.put("fechaRegistro", getFechaActual());
         registro.put("nominaRegistro", "130191");
-        registro.put("tipoEmpleado", 1);
+        registro.put("tipoEmpleado", 0);
 
         // Inserting Row
         db.insert("Empleados", null, registro);
